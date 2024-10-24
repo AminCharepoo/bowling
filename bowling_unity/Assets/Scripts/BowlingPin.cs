f424380215e8875ca0f510b7b5f518d94183c8d7
AminCharepoo <134405766+AminCharepoo@users.noreply.github.com>
2024-10-24 16:45:41 -0400

    remove pins when fallen

--- bowling_unity/Assets/Scripts/BowlingPin.cs
+++ bowling_unity/Assets/Scripts/BowlingPin.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BowlingPin : MonoBehaviour
 {
     private bool isFallen = false;
     public float fallenThreshold = 45f;
-
+    private Rigidbody RB;
+    public float stopMovementThreshold = 0.1F;
+    float elapsedTime = 0f;
+    public float stopTime = 2f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        RB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -22,7 +26,24 @@ public class BowlingPin : MonoBehaviour
             isFallen = true;
             Debug.Log("Pin has fallen!");
             BowlingManager.Instance.AddScore(1);
-            Destroy(gameObject, 2.0f);
+        }
+
+        if (isFallen)
+        {
+            if (RB.velocity.magnitude < stopMovementThreshold)
+            {
+                elapsedTime += Time.deltaTime;
+                
+            }
+            else
+            {
+                elapsedTime = 0f;
+            }
+
+            if (elapsedTime >= stopTime)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
